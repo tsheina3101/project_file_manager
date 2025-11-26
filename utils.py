3f30c6704f7e2005eacfaefb7857fbeceb2975b1
Шеина Татьяна <tsheina@yandex.ru>
2025-11-26 15:30:21 +0500

    Изменение файла util.py

--- utils.py
+++ utils.py
@@ -1,9 +1,12 @@
 # utils.py
 def help():
-    print("Для запуска файлового менеджера наберите команду: python file_manager.py <command> [options]")
-    print("Команды:")
-    print("  copy <source> <destination> - Копировать файл")
-    print("  delete <path> - Удалить файл или папку")
-    print("  count <path> - Подсчитать кол-во файлов в папке")
-    print("  add_date <path>  - Добавить дату создания к имени файла или файлов внутри папки")
-    print("  search <source_dir> <destination_dir> - Поиск папки внутри каталога с выводом пути к нему")
+    print('''
+     ФАЙЛОВЫЙ МЕНЕДЖЕР 
+     Для запуска файлового менеджера наберите команду: python file_manager.py <command> [options]
+     Команды:
+     copy <source> <destination> - Копировать файл
+     delete <path> - Удалить файл или папку
+     count <path> - Подсчитать кол-во файлов в папке
+     add_date <path>  - Добавить дату создания к имени файла или файлов внутри папки
+     search <source_dir> <destination_dir> - Поиск папки внутри каталога с выводом пути к нему
+     ''')
